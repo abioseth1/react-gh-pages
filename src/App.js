4b1759452d0ce59dff474a5b3ef6edfe6c41d1ed
Abiola Abiodun <78275439+abioseth1@users.noreply.github.com>
2021-05-16 20:51:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is just a test for my GH page
         </p>
         <a
           className="App-link"
